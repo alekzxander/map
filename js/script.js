7a4844b072451e9fc3c3d25139bdcaf4cd3d0946
Alexandre Callens <alekzxand@gmail.com>
2017-08-03 12:00:51 +0400

    meuh

---
+++ js/script.js
@@ -0,0 +1,159 @@
+
+
+let tableau =[
+					{	
+						type : 'habitat',
+						lat: -20.904788036065646, lng: 55.498504064567086, popup : '<img src="images/sodiac.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+					{	
+						type : 'habitat',
+						lat: - 20.889987, lng: 55.500152, popup : '<img src="images/sodiac1.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+					{
+						type : 'habitat',
+						lat: -20.873431, lng: 55.452629, popup : '<img src="images/sodiac2.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<br><button>Apercu</button>'},
+					{
+						type : 'habitat',
+						lat: -20.948746, lng: 55.281660, popup : '<img src="images/sodiac3.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+					{
+						type : 'logement',
+						lat: -20.895015, lng:  55.497857, popup : '<img src="images/sodiac4.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+					{
+						type : 'logement',
+						lat: -21.326289, lng: 55.608400, popup : '<img src="images/sodiac5.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+	 				{
+	 					type : 'logement',
+	 					lat: -20.913092, lng: 55.620134, popup : '<img src="images/sodiac6.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta. <button>Apercu</button>'},
+	 				{	
+	 					type : 'logement',
+	 					lat: -20.908671, lng: 55.605084, popup : '<img src="images/sodiac7.jpg" alt=""/></br>Ensembe de bureaux et de commerces dans deux batiments sur une superficie totale de 9 380m2, avec 360 places de parking.<button>Apercu</button>'},
+	 				{
+	 					type : 'amenagement',
+	 					lat: -21.346450, lng: 55.492311, popup : '<img src="images/sodiac8.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+	 				{
+	 					type : 'amenagement',
+	 					lat: -20.890523, lng: 55.443799, popup :'<img src="images/sodiac6.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta. <button>Apercu</button>'},
+ 					{
+ 						type : 'amenagement',
+ 						lat: -21.020964, lng: 55.264664, popup : '<img src="images/sodiac4.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+ 					{
+ 						type : 'amenagement',
+ 						lat: -21.342968, lng: 55.494574, popup : '<img src="images/sodiac2.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<br><button>Apercu</button>'},
+ 					{
+ 						type : 'amenagement',
+ 						lat: -20.888212, lng: 55.469084, popup :'<img src="images/sodiac7.jpg" alt=""/><br><h5>Cour Kerveguen :</h5></br>Ensembe de bureaux et de commerces dans deux batiments sur une superficie totale de 9 380m2, avec 360 places de parking.<button>Apercu</button>'  },
+ 					{
+ 						type : 'projet',
+ 						lat: -21.026939, lng: 55.268634, popup :'<img src="images/sodiac3.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},	
+	 			    {
+	 			    	type : 'projet',
+	 			    	lat: -20.888738, lng: 55.501599, popup : '<img src="images/sodiac.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>'},
+	 			    {
+	 			    	type : 'projet',
+	 			    	lat: -20.931442, lng: 55.298157, popup: '<img src="images/sodiac8.jpg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>' },
+	 			    {
+	 			    	type : 'projet',
+	 			    	lat: -20.907459, lng: 55.503217, popup:'<img src="images/sodiac2.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<br><button>Apercu</button>' },
+	 			    {
+	 			    	type : 'projet',
+	 			    	lat: -20.926038,
+	 			    	 lng: 55.454804,
+	 			    	  popup : '<img src="images/sodiac3.jpeg" alt=""/><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tenetur. Molestias nemo sint, beatae quasi corporis unde sapiente ad, et vitae saepe fugit. Delectus, culpa et minus cupiditate voluptate soluta.<button>Apercu</button>' },
+	 	]		
+
+
+let map = L.map('map').setView([-21.120, 55.532], 9);
+map.scrollWheelZoom.disable();
+
+L.tileLayer("https://api.tiles.mapbox.com/v2/dakno.map-xxbpkb1z/{z}/{x}/{y}.png", {minZoom :9 , maxZoom : 14, 
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+$(document).ready(function(){
+
+	//Activation du zoom de la carte et desactivation au click
+ 	map.on('click', function() {
+	  	if (map.scrollWheelZoom.enabled()) {
+	    	map.scrollWheelZoom.disable();
+	    }else{
+	    	map.scrollWheelZoom.enable();
+	    }
+});
+// $$$$$$$$$$$$FIN de l'activation
+
+
+
+	for(i=0; i < tableau.length; i++){
+		let latitude = tableau[i].lat;
+		let longitute = tableau[i].lng;
+		let type = tableau[i].type;
+		let popup = tableau[i].popup;
+
+let amenagement =  L.layerGroup().addTo(map);
+let logement = L.layerGroup().addTo(map);
+let habitat = L.layerGroup().addTo(map);
+let projet = L.layerGroup().addTo(map);
+		
+
+	if(type === 'habitat'){
+		
+		marker1 = L.marker([latitude, longitute]).bindPopup(popup);	
+		habitat.addLayer(marker1)
+
+	}
+	if(type === 'logement'){
+		marker2 = L.marker([latitude,longitute]).bindPopup(popup);
+		logement.addLayer(marker2)
+	}
+	if(type === 'amenagement'){
+		marker3 = L.marker([latitude,longitute]).bindPopup(popup);
+		amenagement.addLayer(marker3)
+	}
+	if(type === 'projet'){
+		marker4 = L.marker([latitude,longitute]).bindPopup(popup);
+		projet.addLayer(marker4)
+	}
+
+
+$('#check1').on('click',function(){
+
+	if((document).querySelector('#check1').checked ===true){
+		habitat.addTo(map);
+	}else{	
+		map.removeLayer(habitat);
+	}
+});
+$('#check3').on('click',function(){
+	if((document).querySelector('#check3').checked === true){
+		amenagement.addTo(map);
+	}else{
+		map.removeLayer(amenagement);	
+	}
+ })
+$('#check2').on('click',function(){
+	if((document).querySelector('#check2').checked === true){
+		logement.addTo(map);
+	}else{
+		map.removeLayer(logement);	
+	}
+})
+
+
+$('#check4').on('click',function(){
+	if((document).querySelector('#check4').checked === true){
+		projet.addTo(map);
+	}else{
+		map.removeLayer(projet);	
+	}
+})
+
+
+
+
+}//boucle for
+
+	});		
+
+
+		
+		
